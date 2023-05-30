41dd2caa8e38f9e9b5749f68fcb560f41f9fcaa0
chanki <coldair426@gmail.com>
2023-05-30 17:46:00 +0900

    logo edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>coldair</p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
+        <img src='/img/profile.png' className='App-logo' alt='logo' />
+        <p>홍찬기의 포트폴리오</p>
       </header>
     </div>
   );
