2d46868a67a4a611430defc438e01b3c89542582
chanki <coldair426@gmail.com>
2023-06-01 00:15:48 +0900

    install lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import './App.css';
+import { Reset } from 'styled-reset';
+import './style/App.css';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src='/img/profile.png' className='App-logo' alt='logo' />
-        <p>홍찬기의 포트폴리오</p>
-      </header>
-    </div>
+    <>
+      <Reset />
+      <div className='App'>
+        <header className='App-header'>
+          <img src='/img/profile.png' className='App-logo' alt='logo' />
+          <p>홍찬기의 포트폴리오</p>
+        </header>
+      </div>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style/index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
 serviceWorkerRegistration.register();
-
 reportWebVitals();
