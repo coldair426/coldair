c44139412e5641f1899fdb7c7038e79d449fe967
chanki <coldair426@gmail.com>
2023-06-02 23:35:32 +0900

    header 진행중

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,7 +5,17 @@ import classNames from 'classnames/bind';
 const hs = classNames.bind(styles);
 
 function Header() {
-  return <header className={hs('header')}>헤더입니다</header>;
+  return (
+    <header className={hs('header')}>
+      <nav className={hs('header__global-navigation')}>
+        <div className={hs('header__global-navigation--contents')}>
+          <img className={hs('header__global-navigation--logo')} src='/logo192.png' alt='logo' />
+          <div className={hs('header__global-navigation--contents-wrapper')}></div>
+        </div>
+      </nav>
+      <nav className={hs('header__local-navigation')}></nav>
+    </header>
+  );
 }
 
 export default Header;
