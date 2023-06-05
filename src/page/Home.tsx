56580a7fa4bdf76ea0c3308cd289cbce98b5ee63
chanki <coldair426@gmail.com>
2023-06-06 02:08:10 +0900

    Revert "Xxx"
    This reverts commit 135e2e4557d6d14ecb71ef1217d466a3fa85246d.

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import styles from '../style/Home.module.scss';
 import classNames from 'classnames/bind';
 
@@ -9,75 +9,54 @@ function Home({ headerGlobalNavHeight, headerLocalNavHeight }: { headerGlobalNav
   const profileImg = useRef<HTMLImageElement>(null); // "프로필사진."
   const helloPlaceHolder = useRef<HTMLDivElement>(null); // hello place holder
 
-  const [helloPlaceHolderHeight, setHelloPlaceHolderHeight] = useState<number>(0);
-
-  // useEffect(() => {
-  // const headerStyleChange = () => {
-  //   if (header.current && headerLocalNavigation.current && headerLocalNavigationContents.current) {
-  //     if (window.scrollY >= header.current.offsetHeight) {
-  //       headerLocalNavigationContents.current.style.boxShadow = 'none';
-  //       headerLocalNavigation.current.style.boxShadow = '0px 1px 0px 0px rgba(255, 255, 255, 0.24)';
-  //       header.current.style.backgroundColor = 'rgba(29, 29, 31, 0.9)';
-  //       headerLocalNavigation.current.style.backgroundColor = 'rgba(29, 29, 31, 0.9)';
-  //     } else {
-  //       headerLocalNavigationContents.current.style.boxShadow = '0px 1px 0px 0px rgba(255, 255, 255, 0.24)';
-  //       headerLocalNavigation.current.style.boxShadow = 'none';
-  //       header.current.style.backgroundColor = 'transparent';
-  //       headerLocalNavigation.current.style.backgroundColor = 'transparent';
-  //     }
-  //   }
-  // };
-  // window.addEventListener('scroll', headerStyleChange);
-  // return () => {
-  //   window.removeEventListener('scroll', headerStyleChange);
-  // };
-  // }, []);
   useEffect(() => {
-    // 플레이스홀더 높이 동적으로 계산
-    const calculateHelloImgPlaceHolderHeight = () => {
-      if (helloPlaceHolder.current && helloImg.current) {
-        const placeholderHeight =
-          (window.innerHeight - (headerGlobalNavHeight + headerLocalNavHeight) - helloImg.current.offsetHeight) / 2 + (headerGlobalNavHeight + headerLocalNavHeight);
-        setHelloPlaceHolderHeight(placeholderHeight);
-      }
-    };
-    calculateHelloImgPlaceHolderHeight();
-    window.addEventListener('resize', calculateHelloImgPlaceHolderHeight);
-    return () => {
-      window.removeEventListener('resize', calculateHelloImgPlaceHolderHeight);
-    };
-  }, [headerGlobalNavHeight, headerLocalNavHeight]);
+    // const headerStyleChange = () => {
+    //   if (header.current && headerLocalNavigation.current && headerLocalNavigationContents.current) {
+    //     if (window.scrollY >= header.current.offsetHeight) {
+    //       headerLocalNavigationContents.current.style.boxShadow = 'none';
+    //       headerLocalNavigation.current.style.boxShadow = '0px 1px 0px 0px rgba(255, 255, 255, 0.24)';
+    //       header.current.style.backgroundColor = 'rgba(29, 29, 31, 0.9)';
+    //       headerLocalNavigation.current.style.backgroundColor = 'rgba(29, 29, 31, 0.9)';
+    //     } else {
+    //       headerLocalNavigationContents.current.style.boxShadow = '0px 1px 0px 0px rgba(255, 255, 255, 0.24)';
+    //       headerLocalNavigation.current.style.boxShadow = 'none';
+    //       header.current.style.backgroundColor = 'transparent';
+    //       headerLocalNavigation.current.style.backgroundColor = 'transparent';
+    //     }
+    //   }
+    // };
+    // window.addEventListener('scroll', headerStyleChange);
+    // return () => {
+    //   window.removeEventListener('scroll', headerStyleChange);
+    // };
+  }, []);
   useEffect(() => {
     const helloImgMovingEffect = () => {
-      if (helloImg.current && profileImg.current) {
-        // 높이변수
-        const scrollY = window.scrollY;
-        const windowHeight = window.innerHeight;
-        const helloImgHeight = helloImg.current.offsetHeight; // 인사말높이
-        // 스타일변수
-        const helloImgStyle = helloImg.current.style; // 인사말스타일
-        const profileImgStyle = profileImg.current.style; // 프로필사진스타일
+      if (helloImg.current && profileImg.current && helloPlaceHolder.current) {
+        if (window.scrollY < headerGlobalNavHeight * 1) {
+          helloImg.current.style.opacity = '1';
+          helloImg.current.style.top =
+            (
+              (window.innerHeight - (headerGlobalNavHeight + headerLocalNavHeight) - helloImg.current.offsetHeight) / 2 +
+              (headerGlobalNavHeight + headerLocalNavHeight)
+            ).toString() + 'px';
+          helloImg.current.style.width = '250px';
 
-        if (scrollY < headerGlobalNavHeight) {
-          helloImgStyle.opacity = '1';
-          helloImgStyle.top = `${helloPlaceHolderHeight}px`;
-          helloImgStyle.width = '250px';
+          profileImg.current.style.opacity = '0';
 
-          profileImgStyle.opacity = '0';
-        } else if (scrollY >= headerGlobalNavHeight && scrollY < headerGlobalNavHeight * 10) {
-          helloImgStyle.opacity = '1';
-          helloImgStyle.width = '800px';
-          helloImg.current.style.top = `${(windowHeight - headerGlobalNavHeight - helloImgHeight) / 2 + headerGlobalNavHeight}px`;
+          helloPlaceHolder.current.style.height = window.innerHeight + headerGlobalNavHeight * 13 + 'px';
+        } else if (window.scrollY >= headerGlobalNavHeight * 1 && window.scrollY < headerGlobalNavHeight * 10) {
+          helloImg.current.style.opacity = '1';
+          helloImg.current.style.width = '800px';
+          helloImg.current.style.top = ((window.innerHeight - headerGlobalNavHeight - helloImg.current.offsetHeight) / 2 + headerGlobalNavHeight).toString() + 'px';
 
-          profileImgStyle.opacity = '0';
-        } else if (scrollY >= headerGlobalNavHeight * 10 && scrollY < headerGlobalNavHeight * 13) {
-          helloImgStyle.opacity = '0';
+          profileImg.current.style.opacity = '0';
+        } else if (window.scrollY >= headerGlobalNavHeight * 10 && window.scrollY < headerGlobalNavHeight * 13) {
+          helloImg.current.style.opacity = '0';
 
-          profileImgStyle.opacity = '0';
-        } else if (scrollY >= headerGlobalNavHeight * 13) {
-          profileImgStyle.opacity = '1';
-        } else if (scrollY >= headerGlobalNavHeight * 18) {
-          profileImgStyle.opacity = '0';
+          profileImg.current.style.opacity = '0';
+        } else if (window.scrollY >= headerGlobalNavHeight * 13) {
+          profileImg.current.style.opacity = '1';
         }
       }
     };
@@ -86,7 +65,7 @@ function Home({ headerGlobalNavHeight, headerLocalNavHeight }: { headerGlobalNav
     return () => {
       window.removeEventListener('scroll', helloImgMovingEffect);
     };
-  }, [headerGlobalNavHeight, headerLocalNavHeight, helloPlaceHolder, helloPlaceHolderHeight]);
+  }, [headerGlobalNavHeight, headerLocalNavHeight]);
 
   return (
     <>
@@ -95,7 +74,7 @@ function Home({ headerGlobalNavHeight, headerLocalNavHeight }: { headerGlobalNav
           <img className={hs('home__hello--img')} ref={helloImg} src='/img/Home__hello.png' alt='say-hello' />
           <img className={hs('home__profile--img')} ref={profileImg} src='/img/Home__profile.png' alt='profile-of-me' />
         </div>
-        <div className={hs('home__hello--place-holder')} ref={helloPlaceHolder} />
+        <div className={hs('home__hello-place-holder')} ref={helloPlaceHolder}></div>
       </div>
       <div>플레이스홀더 이후</div>
     </>
